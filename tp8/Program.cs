c545623c279658dd19dd0bbdd30cb7271bccb6cc
VelizMiguelC <miniveliz14@gmail.com>
2023-06-12 11:28:14 -0300

    punto 2

--- tp8/Program.cs
+++ tp8/Program.cs
@@ -1,115 +1,39 @@
-﻿﻿using tarea;
-internal class Program
+﻿﻿internal class Program
 {
     private static void Main(string[] args)
     {
-        static int ValidaIngreso()
-        {
-            int dato = 0;
-            do
-            {
-                System.Console.WriteLine("Ingrese el numero de la id de la tarea que se realizo");
-            } while (!int.TryParse(System.Console.ReadLine(), out dato));
-            return dato;
-        }
-        string[] descripciones = {
-    "Lavar ropa",
-    "Cocinar",
-    "Limpiar",
-    "Ordenar",
-    "Barrer",
-    "Regar",
-    "Planchar",
-    "Pasear",
-    "Cuidar",
-    "Comprar",
-    "Bañar",
-    "Cortar",
-    "Secar",
-    "Fregar",
-    "Cocer",
-    "Tender",
-    "Sobar",
-    "Ordenar",
-    "Doblar",
-    "Podar"
-};
+        // Solicitar al usuario que ingrese el path de la carpeta
+        Console.WriteLine("Ingrese el path de la carpeta:");
+        string carpetaPath = Console.ReadLine();
 
+        // Verificar si el directorio existe
+        if (Directory.Exists(carpetaPath))
+        {
+            // Obtener la lista de archivos en la carpeta
+            string[] archivos = Directory.GetFiles(carpetaPath);
 
-        int cantTareas = 5;
-        List<Tarea> pendientes = new List<Tarea>();
-        List<Tarea> realizadas = new List<Tarea>();
-        for (int i = 0; i < cantTareas; i++)
-        {
-            Random random = new Random();
-            int numDescripcion = random.Next(0, 20);
-            var tareanueva = new Tarea(i, descripciones[numDescripcion]);
-            pendientes.Add(tareanueva);
-        }
-        void MostrarTareas(List<Tarea> lista)
-        {
-            int cantTareas = lista.Count;
-            for (int i = 0; i < cantTareas; i++)
+            // Crear el archivo CSV "index.csv"
+            string csvFilePath = Path.Combine(carpetaPath, "index.csv");
+            using (StreamWriter writer = new StreamWriter(csvFilePath))
             {
-                System.Console.WriteLine("La tarea numero " + lista[i].Id);
-                System.Console.WriteLine("La descripcion de la tarea es:" + lista[i].Descripcion);
-                System.Console.WriteLine("La duracion de la tarea es:" + lista[i].Duracion);
-            }
-        }
-        System.Console.WriteLine("--------Tareas Pendientes--------");
-        MostrarTareas(pendientes);
-        System.Console.WriteLine("Se realizo alguna tarea?");
-        string mover = System.Console.ReadLine();
-        while (mover == "si")
-        {
-            int idBusca = ValidaIngreso();
-            var tareaCopiada = pendientes.Find(t => t.Id == idBusca);
-            realizadas.Add(tareaCopiada);
-            pendientes.Remove(tareaCopiada);
-            System.Console.WriteLine("--------Tareas Pendientes--------");
-            MostrarTareas(pendientes);
-            System.Console.WriteLine("--------Tareas Realizadas--------");
-            MostrarTareas(realizadas);
-            System.Console.WriteLine("Se realizo alguna otra tarea?");
-            mover = System.Console.ReadLine();
-        }
-        System.Console.WriteLine("Desea Buscar alguna tarea por descripcion");
-        mover = System.Console.ReadLine();
-        if (string.Equals(mover, "si"))
-        {
-            buscartareas(pendientes);
-        }
-        void buscartareas(List<Tarea> lista)
-        {
-            System.Console.WriteLine("Ingrese la descripción de la tarea que busca:");
-            string descrip = System.Console.ReadLine();
-            var tareaBuscada = lista.Find(t => t.Descripcion == descrip);
+                // Escribir las cabeceras en el archivo CSV
+                writer.WriteLine("Índice,Nombre,Extensión");
 
-            if (tareaBuscada != null)
-            {
-                System.Console.WriteLine("Tarea encontrada:");
-                System.Console.WriteLine("ID: " + tareaBuscada.Id);
-                System.Console.WriteLine("Descripción: " + tareaBuscada.Descripcion);
-                System.Console.WriteLine("Duración: " + tareaBuscada.Duracion);
+                // Recorrer los archivos y escribirlos en el archivo CSV
+                for (int i = 0; i < archivos.Length; i++)
+                {
+                    string nombreArchivo = Path.GetFileName(archivos[i]);
+                    string extensionArchivo = Path.GetExtension(archivos[i]);
+                    writer.WriteLine($"{i + 1},{nombreArchivo},{extensionArchivo}");
+                }
             }
-            else
-            {
-                System.Console.WriteLine("No se encontró ninguna tarea con la descripción especificada.");
-            }
+
+            Console.WriteLine("Se ha creado el archivo CSV \"index.csv\" con el listado de archivos.");
         }
-        int sumar(List<Tarea> lista)
+        else
         {
-            int suma = 0;
-            for (int i = 0; i < lista.Count; i++)
-            {
-                suma += lista[i].Duracion;
-            }
-            return suma;
+            Console.WriteLine("El directorio ingresado no existe.");
         }
-        int suma = sumar(realizadas);
-        var archivo = new StreamWriter("horastrabajadas.txt");
-        archivo.WriteLine("Cantidad de Tareas realizadas:" + suma);
-        archivo.Close();
     }
 
 }
